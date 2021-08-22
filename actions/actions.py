821c1b10e72b29071f2ff957beb920b6891bfe94
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-08-22 14:25:09 +0700

    add actions

--- actions/actions.py
+++ actions/actions.py
@@ -4,7 +4,11 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any,Text,Dict,List
 
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 # This is a simple example for a custom action which utters "Hello World!"
 
 # from typing import Any, Text, Dict, List
@@ -25,3 +29,32 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionReceiveName(Action):
+
+    def name(self) -> Text:
+        return "action_receive_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        text = tracker.latest_message['text']
+        dispatcher.utter_message(text=f"Aku akan mengingat namamu {text}!")
+        return [SlotSet("name", text)]
+
+class ActionSayName(Action):
+
+    def name(self) -> Text:
+        return "action_say_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        name = tracker.get_slot("name")
+        if not name:
+            dispatcher.utter_message(text="Maaf, Aku gak ingat namamu.")
+        else:
+            dispatcher.utter_message(text=f"Nama mu adalah {name}!")
+        return []
